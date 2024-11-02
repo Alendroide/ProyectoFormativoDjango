e25aaccd286376ba4bf356a2e92bf541b7112f69
Juniorr-7 <juniorfajardom@gmail.com>
2024-11-02 12:45:35 -0500

    Modulo Usuarios creado

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    username = None
+    identificacion = models.BigIntegerField(unique=True, null=False)
+    nombre = models.CharField(max_length=30)
+    apellidos = models.CharField(max_length=30)
+    fechaNacimiento = models.DateField()
+    telefono = models.CharField(max_length=15)
+    correoElectronico = models.EmailField(max_length=255, unique=True)
+    admin = models.BooleanField(default=False, null=False)
+
+    USERNAME_FIELD = 'correoElectronico'
+    REQUIRED_FIELDS = ['identificacion', 'nombre', 'apellidos', 'fechaNacimiento', 'telefono']
+
+    def __str__(self):
+        return str(self.identificacion)
