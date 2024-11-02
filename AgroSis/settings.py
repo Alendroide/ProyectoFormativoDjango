f4465c7a68d6f8f279517888d1e597b42d2f30ba
Alendroide <alendroideyt@gmail.com>
2024-11-02 14:24:25 -0500

    PostGreSQL database configured

--- AgroSis/settings.py
+++ AgroSis/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'AgroSis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'AgroSisDjango',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
