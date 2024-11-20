89be1330850ec54f0db68adfd1ecff426e533a68
karensemanate <karen.semanate@soy.sena.edu.co>
2024-11-19 19:13:02 -0500

    modificacion en las urls

--- AgroSis/urls.py
+++ AgroSis/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from apps.electronica.api.router import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
-    
+    path('admin/', admin.site.urls),
+    path('api/', include(router_lote.urls)),
+    path('api/', include(router_era.urls)),
+    path('api/', include(router_sensor.urls)),
+    path('api/', include(router_HumedadTerreno.urls)),
+    path('api/', include(router_ph.urls))
+
 ]
