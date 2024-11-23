69cd20bebc65a735298010cbc11f25c22d809749
karensemanate <karen.semanate@soy.sena.edu.co>
2024-11-22 20:28:15 -0500

    Se creó el método connect, que se activa cuando un cliente se conecta al websocket. En este momento, se define un grupo llamado sensor_updates, al que se unen todos los websockets de los clientes conectados. Esto nos permitirá enviar actualizaciones a todos los clientes de manera simultánea.

---
+++ apps/electronica/api/consumers.py
@@ -0,0 +1,14 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from apps.electronica.models import Sensor
+
+class SensorConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = 'sensor_updates'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        await self.accept()  
