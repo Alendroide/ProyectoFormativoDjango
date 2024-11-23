851c48480ff9d223eb07d117d88654158da4b2e9
karensemanate <karen.semanate@soy.sena.edu.co>
2024-11-23 11:31:20 -0500

    Se reorganizó el archivo de la API electronica. Dentro de la carpeta api cada componente tiene su propia carpeta models,routers, serailizers, viewsy consumers. Esta reestructuració hace que el codigo sea mas ordenado y permita que cada elemento este separado y en su lugar

--- apps/electronica/api/router.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from apps.electronica.api.views import *
-
-router_Electronica = DefaultRouter()
-router_Electronica.register(prefix= 'lote', viewset= LoteView, basename='lote')
-router_Electronica.register(prefix='eras', viewset=Erasview, basename='eras')
-router_Electronica.register(prefix='sensor',viewset=sensoresview, basename='sensor')
-router_Electronica.register(prefix='HumedadTerreno', viewset=HumedadTerrenoview, basename='HumedadTerreno')
-router_Electronica.register(prefix='ph', viewset=Phview, basename='ph')
