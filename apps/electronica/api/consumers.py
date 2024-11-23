6561286d7614fe49ab3368a9990be26ea3e1a32b
karensemanate <karen.semanate@soy.sena.edu.co>
2024-11-22 20:33:24 -0500

    Se creó el metodo disconnect, la cual se ejecuta cuando el cliente se desconecta. Simplemente elimina al cliente del grupo de WebSocket

--- apps/electronica/api/consumers.py
+++ apps/electronica/api/consumers.py
@@ -12,3 +12,25 @@ class SensorConsumer(AsyncWebsocketConsumer):
         )
 
         await self.accept()  
+
+    async def receive(self, text_data):
+
+        text_data_json = json.loads(text_data)
+        sensor_id = text_data_json['sensor_id']
+        sensor_value = text_data_json['valor']
+
+        try:
+            sensor = Sensor.objects.get(id=sensor_id)
+            sensor.valor = sensor_value
+            sensor.save()
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'sensor_update',
+                    'sensor_id': sensor.id,
+                    'valor': sensor.valor,
+                }
+            )
+        except Sensor.DoesNotExist:
+            pass
