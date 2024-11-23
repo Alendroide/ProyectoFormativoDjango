b3e0e09a897509ce20c45cf74bcdb64278c8bf32
karensemanate <karen.semanate@soy.sena.edu.co>
2024-11-22 19:28:02 -0500

    Mejorar manejo de relaciones entre modelos

--- apps/electronica/api/serializers.py
+++ apps/electronica/api/serializers.py
@@ -1,27 +1,24 @@
 from rest_framework.serializers import ModelSerializer
 from apps.electronica.models import *
+from rest_framework import serializers
 
 class LoteSerializer(ModelSerializer):
     class Meta:
         model = Lote
-        fields = '__all__'
+        fields = ['id', 'nombre','descripcion','tamX','tamY','estado','posX','posY']
 
 class ErasSerializer(ModelSerializer):
+    fk_lote = LoteSerializer(read_only=True)
+    fk_lote_id = serializers.PrimaryKeyRelatedField(
+        queryset = Lote.objects.all(),source = 'fk_lote', write_only=True 
+    )
     class Meta:
         model = Eras
-        fields = '__all__'
+        fields = ['id','fk_lote','tipo','fk_lote_id', 'nombre','descripcion','tamX','tamY','posX','posY']
 
 class SensorSerializer(ModelSerializer):
     class Meta:
         model = Sensor
         fields = '__all__'
 
-class HumedadTerrenoSerializer(ModelSerializer):
-    class Meta:
-        model = HumedadTerreno
-        fields = '__all__'
 
-class PhSerializer(ModelSerializer):
-    class Meta:
-        model = PH
-        fields = '__all__'
--- apps/electronica/models.py
+++ apps/electronica/models.py
@@ -20,67 +20,57 @@ class Lote(models.Model):
             radiacion_solar = self.sensores.filter(tipo='LUM').latest('fecha').valor  
             velocidad_viento = self.sensores.filter(tipo='VIE').latest('fecha').valor  
 
-           
             velocidad_viento = velocidad_viento * 1000 / 3600  
             radiacion_solar = radiacion_solar * 0.0864  
 
-            elevacion = 1000 
+            elevacion = 1000
             es = 0.6108 * math.exp((17.27 * temperatura) / (temperatura + 237.3))
             ea = es * (humedad_relativa / 100)
-            delta = (4098 * es) / ((temperatura + 237.3) ** 2) 
+            delta = (4098 * es) / ((temperatura + 237.3) ** 2)
             gamma = 0.665 * 10 ** -3 * (101.3 * ((293 - (0.0065 * elevacion)) / 293) ** 5.26)
 
             ET0 = (0.408 * delta * radiacion_solar + gamma * (900 / (temperatura + 273)) * velocidad_viento * (es - ea)) \
                 / (delta + gamma * (1 + 0.34 * velocidad_viento))
 
-            return round(ET0, 2)  
-        except Sensor.DoesNotExist:
+            return round(ET0, 2)
+        except self.sensores.model.DoesNotExist:
             return "No hay datos suficientes para calcular la evapotranspiración."
 
 
 class Eras(models.Model):
-    fk_lote = models.ForeignKey(Lote, on_delete=models.SET_NULL, null=True, related_name='eras')
+    fk_lote = models.ForeignKey(Lote, on_delete=models.SET_NULL, null=True) 
+    tipo = models.CharField(max_length=20)
     tamX = models.DecimalField(max_digits=6, decimal_places=2)
     tamY = models.DecimalField(max_digits=6, decimal_places=2)
     posX = models.DecimalField(max_digits=6, decimal_places=2)
     posY = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"Bancal en {self.fk_lote.nombre}"
+        return f"Era {self.tipo} en {self.fk_lote.nombre if self.fk_lote else 'sin lote'}"
 
 
-# Modelo general para sensores
 class Sensor(models.Model):
     SENSOR_TYPES = [
         ('TEM', 'Temperatura'),
         ('LUM', 'Iluminación'),
         ('HUM_A', 'Humedad Ambiental'),
         ('VIE', 'Velocidad del Viento'),
+        ('HUM_T', 'Humedad del Terreno'),
+        ('PH', 'Nivel de PH')
     ]
 
-    fk_lote = models.ForeignKey(Lote, on_delete=models.SET_NULL, null=True, related_name='sensores')
+    fk_lote = models.ForeignKey(Lote, on_delete=models.SET_NULL, null=True, related_name='sensores', blank=True)
+    fk_eras = models.ForeignKey(Eras, on_delete=models.SET_NULL, null=True, related_name='sensores', blank=True)
     fecha = models.DateTimeField(auto_now_add=True)
     tipo = models.CharField(max_length=6, choices=SENSOR_TYPES)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         tipo_sensor = dict(self.SENSOR_TYPES).get(self.tipo, 'Desconocido')
-        return f"{tipo_sensor}: {self.valor} en {self.fk_lote.nombre}"
-
-
-class HumedadTerreno(models.Model):
-    fk_Eras = models.ForeignKey(Eras, on_delete=models.SET_NULL, null=True)
-    fecha = models.DateTimeField(auto_now_add=True)
-    porcentaje = models.PositiveIntegerField()
-
-    def __str__(self):
-        return f"Humedad Terreno: {self.porcentaje}% en Bancal {self.fk_Eras.fk_lote.nombre}"
-
-
-class PH(models.Model):
-    fk_Eras = models.ForeignKey(Eras, on_delete=models.CASCADE, related_name='ph_readings')
-    fecha = models.DateTimeField(auto_now_add=True)
-    valor = models.DecimalField(max_digits=4, decimal_places=2)
-
-    def __str__(self):
-        return f"pH: {self.valor} en Bancal {self.fk_Eras.fk_lote.nombre} (Fecha: {self.fecha.strftime('%Y-%m-%d %H:%M')})"
+        if self.fk_eras:
+            ubicacion = self.fk_eras.fk_lote.nombre
+        elif self.fk_lote:
+            ubicacion = self.fk_lote.nombre
+        else:
+            ubicacion = 'Sin ubicación'
+        return f"{tipo_sensor}: {self.valor} en {ubicacion}"
