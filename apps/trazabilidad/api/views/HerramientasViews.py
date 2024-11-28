3dbb1c8ce7a64c0e84b35ff7ec2b4962ac5755b9
Mamuel17 <rubyesperanza596@gmail.com>
2024-11-28 18:04:02 -0500

    Set de vistas de Herramientas corregido, error en el nombre

--- apps/trazabilidad/api/views/HerramientasViews.py
+++ apps/trazabilidad/api/views/HerramientasViews.py
@@ -2,6 +2,6 @@ from rest_framework.viewsets import ModelViewSet
 from ..models.HerramientasModel import Herramientas
 from ..serializers.HerramientasSerializer import HerramientasSerializer
 
-class EspeciesViewSet(ModelViewSet):
+class HerramientasViewSet(ModelViewSet):
     queryset = Herramientas.objects.all()
     serializer_class = HerramientasSerializer
