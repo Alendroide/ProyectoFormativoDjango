7627ae116f538f23b3436c2e422c5670c640ba64
Alendroide <alendroideyt@gmail.com>
2024-11-28 20:47:13 -0500

    Corrections made to 'Trazabilidad' and 'Electronica' modules, migration now possible

--- apps/trazabilidad/api/models/UsosHerramientasModel.py
+++ apps/trazabilidad/api/models/UsosHerramientasModel.py
@@ -1,8 +1,8 @@
 from django.db import models
-import HerramientasModel
+from .HerramientasModel import Herramientas
 
 class UsosHerramientas(models.Model):
-    fk_Herramienta = models.ForeignKey(HerramientasModel,on_delete=models.SET_NULL,null=True)
+    fk_Herramienta = models.ForeignKey(Herramientas,on_delete=models.SET_NULL,null=True)
     fk_Actividad = models.IntegerField()
     def __str__(self):
         return str(self.fk_Herramienta) + str(self.fk_Actividad)
