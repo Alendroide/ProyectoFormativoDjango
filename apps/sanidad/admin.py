baa52f7977282d2f3b53eb40ab2cc588f393e45a
MaiberCordoba <cordobamaiber@gmail.com>
2024-11-29 00:23:26 -0500

    agregando registrando el modelo plaga en el admin

--- apps/sanidad/admin.py
+++ apps/sanidad/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from apps.sanidad.api.models.tipoPlaga import tipoPlaga
+from apps.sanidad.api.models.PlagaModel import Plaga
 
 admin.site.register(tipoPlaga)
+admin.site.register(Plaga)
