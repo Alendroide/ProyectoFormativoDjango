a1b539321de9a90f0f290498f776141663412b97
advg <advguwu.09.02.2005@gmail.com>
2024-11-28 20:21:08 -0500

    Creacion de carpeta models en el modulo finanzas para crear un modelo individual

--- apps/finanzas/models.py
+++ apps/finanzas/models.py
@@ -1,11 +1,7 @@
 from django.db import models
 from apps.users.models import Usuario
 # Modelo de la tabla Tipos de desechos. 
-class TiposDesecho(models.Model):
-    nombre = models.CharField(max_length=50)
-    descripcion = models.TextField(max_length=200)
-    def __str__(self):
-        return self.nombre
+
     
 class Cultivos(models.Model):
     nombre = models.CharField(max_length=30)
