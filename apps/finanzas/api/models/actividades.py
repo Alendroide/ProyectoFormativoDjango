05bce14c17aa00ddc8ffcb5f2e518d7663afd5ff
advg <advguwu.09.02.2005@gmail.com>
2024-11-28 21:18:02 -0500

    Agrego un estado al modelo actividades

--- apps/finanzas/api/models/actividades.py
+++ apps/finanzas/api/models/actividades.py
@@ -3,10 +3,16 @@ from apps.finanzas.api.models.cultivos import Cultivos
 from apps.users.models import Usuario
 
 class Actividades(models.Model):
+    ESTADO_CHOICES = [
+        ('AS',"Asignada"),
+        ('CO',"Proceso"),
+        ('CA',"Finalizada")
+    ]
     fk_Cultivo = models.ForeignKey(Cultivos, on_delete = models.SET_NULL, null= True)
     fk_Usuario=models.ForeignKey(Usuario, on_delete= models.SET_NULL,null=True)
     titulo = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=200)
     fecha =models.DateField(auto_now=False)
+    estado=models.CharField(max_length=3,choices=ESTADO_CHOICES)
     def __str__(self):
         return self.titulo
