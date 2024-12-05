ec091d77d2ea0ee91bacd0a993d7f1acc1d85874
advg <advguwu.09.02.2005@gmail.com>
2024-12-04 20:17:35 -0500

    Router para serializer Cultivos

---
+++ apps/finanzas/api/routers/routerCultivos.py
@@ -0,0 +1,5 @@
+from rest_framework.routers import DefaultRouter
+from apps.finanzas.api.serializers.serializerCultivos import SerializerCultivos
+
+routerCultivos = DefaultRouter()
+routerCultivos.register(prefix="/cultivos",viewset=SerializerCultivos)
