21aa0b81d773d71017188a599e30fa17a785344d
MaiberCordoba <cordobamaiber@gmail.com>
2024-12-04 23:14:23 -0500

    registrando UsoProductosControl en el apartado de admin

--- apps/sanidad/admin.py
+++ apps/sanidad/admin.py
@@ -5,6 +5,7 @@ from apps.sanidad.api.models.AfeccionesMoldel import Afecciones;
 from apps.sanidad.api.models.TiposControlModel import TiposControl;
 from apps.sanidad.api.models.controlesModel import Controles;
 from apps.sanidad.api.models.ProductosControlModel import ProductosControl;
+from apps.sanidad.api.models.UsoProductosControlModel import UsoProductosControl;
 
 
 admin.site.register(tipoPlaga)
@@ -13,3 +14,4 @@ admin.site.register(Afecciones)
 admin.site.register(TiposControl)
 admin.site.register(Controles)
 admin.site.register(ProductosControl)
+admin.site.register(UsoProductosControl)
