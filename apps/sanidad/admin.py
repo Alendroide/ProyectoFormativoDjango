fad7102f7deeae92aa6c777284a8ebc37ca5096a
MaiberCordoba <cordobamaiber@gmail.com>
2024-12-04 20:25:04 -0500

    agregando el modelo afecciones a admin.py para observar si es posible realizar insersiones desde el apartado del admin que ofrece django

--- apps/sanidad/admin.py
+++ apps/sanidad/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from apps.sanidad.api.models.tipoPlaga import tipoPlaga
 from apps.sanidad.api.models.PlagaModel import Plaga
+from apps.sanidad.api.models.AfeccionesMoldel import Afecciones
+
 
 admin.site.register(tipoPlaga)
 admin.site.register(Plaga)
+admin.site.register(Afecciones)
