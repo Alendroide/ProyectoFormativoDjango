8493c5ca4f3affe05e63c1b87e3ba4bc547f8253
MaiberCordoba <cordobamaiber@gmail.com>
2024-12-04 21:59:07 -0500

    creando el modelo controles, importandolo en models de sanidad y realizando la migracion correspondiente

--- apps/sanidad/models.py
+++ apps/sanidad/models.py
@@ -3,6 +3,7 @@ from apps.sanidad.api.models.tipoPlaga import tipoPlaga;
 from apps.sanidad.api.models.PlagaModel import Plaga;
 from apps.sanidad.api.models.AfeccionesMoldel import Afecciones;
 from apps.sanidad.api.models.TiposControlModel import TiposControl;
+from apps.sanidad.api.models.controlesModel import Controles;
 
 
 
