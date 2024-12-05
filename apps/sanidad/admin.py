1f50f8fd84bcb80c5460e27ebd0b520885f16290
MaiberCordoba <cordobamaiber@gmail.com>
2024-12-04 21:48:24 -0500

    agregando el modelo tipos control a admin

--- apps/sanidad/admin.py
+++ apps/sanidad/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from apps.sanidad.api.models.tipoPlaga import tipoPlaga
 from apps.sanidad.api.models.PlagaModel import Plaga
 from apps.sanidad.api.models.AfeccionesMoldel import Afecciones
+from apps.sanidad.api.models.TiposControlModel import TiposControl
 
 
 admin.site.register(tipoPlaga)
 admin.site.register(Plaga)
 admin.site.register(Afecciones)
+admin.site.register(TiposControl)
