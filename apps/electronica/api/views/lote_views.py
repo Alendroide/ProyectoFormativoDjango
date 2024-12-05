5e92c3c24698200407c12f5ef9d17bc90236265a
karensemanate <karen.semanate@soy.sena.edu.co>
2024-12-04 19:54:37 -0500

    se agrego from rest_framework.permissions import IsAuthenticated, para  manejar con tokens, sesiones o cualquier otro método de autenticación configurado en el proyecto.

--- apps/electronica/api/views/lote_views.py
+++ apps/electronica/api/views/lote_views.py
@@ -5,12 +5,14 @@ from rest_framework import status
 from apps.electronica.api.models.lote import Lote
 from apps.electronica.api.models.sensor import Sensor
 from apps.electronica.api.serializers.Lote_Serializer import LoteSerializer
+from rest_framework.permissions import IsAuthenticated
 from decimal import Decimal
 import math
 
 class LoteView(ModelViewSet):
     queryset = Lote.objects.all()
     serializer_class = LoteSerializer
+    permission_classes = [IsAuthenticated]
 
     @action(detail=True, methods=['get'])
     def calcular_evapotranspiracion(self, request, pk=None):
